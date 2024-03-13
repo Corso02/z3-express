f4e4b225f880a25d1c15c32ad3689a08cccad161
Peter Vanát <vanat.peter@gmail.com>
2024-03-13 23:36:38 +0100

    jooj

--- app.js
+++ app.js
@@ -9,19 +9,37 @@ async function getCtx(){
     return res.Context
 }
 
-app.get("/", (req, res) => {
 
+app.get("/", (req, res) => {
+    console.log("neheheh")
 })
 
 app.get("/solve", async (req, res) => {
     let Context = await getCtx()
-    const {Solver, Int, And} = new Context("main")
+    const {Solver, Int, And, Implies, BitVec} = new Context("main")
     
     const x = Int.const('x');
     const solver = new Solver();
-    solver.add(And(x.ge(0), x.le(9)));
+    //solver.add(And(x.ge(0), x.le(9)));
+    //console.log(solver.ctx.ast_from_string.toString())
+    //console.log(solver.ctx.ast_from_string("(k = 5 => k + 1 = 6)"))
+
+    const k = Int.const("k")
+
+    const nehehe = "true => false"
+
+    let parts = nehehe.split("=>").map(p => p.split(" ").map(c => c == "=" ? "==" : c).join(""))
+    console.log(parts) // k == 5, k + 1 == 6
+
+
+    const expr = `solver.add(Implies(${parts[0]}, ${parts[1]}))`
+
+    eval(`${expr}`) // abomination
+
     let result = await solver.check(); 
-    
+
+    console.log(result)    
+
     res.send(`res: ${result}`)
 })
 
